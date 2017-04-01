00cd29517b36ba8f63475a13f8eb9d6fcb04b736
deborahak <deborahkain@mac.com>
2017-04-01 07:42:05 -0700

    my solution

--- server.js
+++ server.js
@@ -52,8 +52,6 @@ app.delete('/shopping-list/:id', (req, res) => {
   console.log(`Deleted shopping list item \`${req.params.id}\``);
   res.status(204).end();
 });
-
-
 // when new recipe added, ensure has required fields. if not,
 // log error and return 400 status code with hepful message.
 // if okay, add new item, and return it with a status 201.
@@ -72,10 +70,15 @@ app.post('/recipes', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
-
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
-})
+});
+
+app.delete('/recipes/:id', (req, res) =>) {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted recipe \`${req.params.id}\``);
+  res.status(204).end();
+}
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
